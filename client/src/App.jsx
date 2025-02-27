3254e7ecb7dbe679bca40e35bea860627860dfdf
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-27 23:29:33 +0530

    delete extra css and login file initialized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import { Button } from './components/ui/button'
+import Login from './pages/Login'
 
 function App() {
 
   return (
-    <Button>Let's build LMS</Button>
+    <main>
+    <Login/>
+    </main>
   )
 }
 
