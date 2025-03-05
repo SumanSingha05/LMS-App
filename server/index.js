8293dd24b3161cab21f8d940f275985a7a8061a4
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-05 22:49:53 +0530

    Database connection done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/db.js"
 
+dotenv.config({});
+
+// call database connection here
+connectDB;
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`);
