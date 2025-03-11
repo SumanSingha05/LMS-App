00366a4165fdb6a7fe06989e3dfb451132dc18f6
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-11 19:57:05 +0530

    Navbar changes done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,26 @@
 import { Icon, Menu, School } from 'lucide-react'
 import React from 'react'
 import { Button } from './ui/button';
-import { 
-    DropdownMenu, 
-    DropdownMenuContent, 
-    DropdownMenuGroup, 
-    DropdownMenuItem, 
-    DropdownMenuLabel,  
-    DropdownMenuSeparator,  
-    DropdownMenuTrigger } from './ui/dropdown-menu';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from './ui/dropdown-menu';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import DarkMode from '@/DarkMode';
-import { 
+import {
     Sheet,
     SheetClose,
     SheetContent,
-    SheetDescription,
     SheetFooter,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
- } from './ui/sheet';
-import { Label } from './ui/label';
-import { Input } from './ui/input';
+} from './ui/sheet';
 import { Separator } from '@radix-ui/react-dropdown-menu';
 
 const Navbar = () => {
@@ -73,13 +71,13 @@ const Navbar = () => {
                                 <Button>Signup</Button>
                             </div>
                         )}
-                        <DarkMode/>
+                    <DarkMode />
                 </div>
             </div>
             {/* Mobile Device */}
             <div className='flex md:hidden items-center justify-between px-4 h-full'>
-            <h1 className='font-extrabold text-2xl'>E-learning</h1>
-              <MobileNavbar/>
+                <h1 className='font-extrabold text-2xl'>E-learning</h1>
+                <MobileNavbar />
             </div>
 
         </div>
@@ -90,31 +88,35 @@ export default Navbar;
 
 
 const MobileNavbar = () => {
+    const role = "instructor"
     return (
         <Sheet>
-      <SheetTrigger asChild>
-        <Button size='icon' className="rounded-full bg-gray-300 hover:bg-gray-200" variant="outline">
-          <Menu/>
-        </Button>
-      </SheetTrigger>
-      <SheetContent className="flex flex-col">
-        <SheetHeader className="flex flex-row items-center justify-between mt-2">
-          <SheetTitle>E-Learning</SheetTitle>
-          <DarkMode/>
-        </SheetHeader>
-        <Separator className='mr-2'/>
-        <nav className='flex flex-col space-y-4'>
-        <span>My Learning</span>
-        <span>Edit Profile</span>
-        <span>Logout</span>
-        </nav>
-        
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
-      </SheetContent>
-    </Sheet>
+            <SheetTrigger asChild>
+                <Button size='icon' className="rounded-full bg-gray-300 hover:bg-gray-200" variant="outline">
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent className="flex flex-col">
+                <SheetHeader className="flex flex-row items-center justify-between mt-2">
+                    <SheetTitle>E-Learning</SheetTitle>
+                    <DarkMode />
+                </SheetHeader>
+                <Separator className='mr-2' />
+                <nav className='flex flex-col space-y-4'>
+                    <span>My Learning</span>
+                    <span>Edit Profile</span>
+                    <span>Logout</span>
+                </nav>
+                {
+                    role === "instructor" && (
+                        <SheetFooter>
+                            <SheetClose asChild>
+                                <Button type="submit">Dashboard</Button>
+                            </SheetClose>
+                        </SheetFooter>
+                    )
+                }
+            </SheetContent>
+        </Sheet>
     )
 }
