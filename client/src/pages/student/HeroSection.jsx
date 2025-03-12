9f4510f5e9ff49beb8a6603a01a9cc1454187b0c
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-12 16:30:39 +0530

    Basic HeroSection added

---
+++ client/src/pages/student/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='relative bg-gradient-to-r from-blue-500 to bg-indigo-600 dark:from-gray-800 dark:to-gray-900 py-16 px-4 text-center'>
+        <div className='max-2-3xl mx-auto'>
+        <h1 className='text-white text-4xl font-bold mb-4'>Find the Best courses for you </h1>
+        <p className='text-gray-200 dark:text-gray-400 mb-8'>Discover, Learn and Upskill our wide range of courses</p>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
