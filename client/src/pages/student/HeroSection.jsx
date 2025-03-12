e84ef55cc54e0d1a05eb4c886b84a0e75d145f3a
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-12 16:42:05 +0530

    Typing search bar added successfully in the hero section.

--- client/src/pages/student/HeroSection.jsx
+++ client/src/pages/student/HeroSection.jsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const HeroSection = () => {
@@ -6,6 +8,14 @@ const HeroSection = () => {
         <div className='max-2-3xl mx-auto'>
         <h1 className='text-white text-4xl font-bold mb-4'>Find the Best courses for you </h1>
         <p className='text-gray-200 dark:text-gray-400 mb-8'>Discover, Learn and Upskill our wide range of courses</p>
+        
+        <form action="" className='flex items-center bg-white dark:bg-gray-800 rounded-full shadow-lg overflow-hidden max-w-xl mx-auto mb-6'>
+          <Input
+          type="text"
+          className="flex-grow border-none focus-visible:ring-0 px-6 py-3 text-gray-900 placeholder-gray-400 dark:placeholder-gray-500"
+          />
+          <Button className="bg-blue-600 dark:bg-blue-700 text-white px-6 py-3 rounded-r-full hover:bg-blue-700 dark:hover:bg-blue-800">Search</Button>
+        </form>
         </div>
     </div>
   )
