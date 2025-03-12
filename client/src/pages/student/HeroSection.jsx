02f8b9c899d32ec5d6287f3defb172503328829b
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-12 16:49:33 +0530

    Explore courses button added successfully in the Hero section component.

--- client/src/pages/student/HeroSection.jsx
+++ client/src/pages/student/HeroSection.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className='relative bg-gradient-to-r from-blue-500 to bg-indigo-600 dark:from-gray-800 dark:to-gray-900 py-16 px-4 text-center'>
+    <div className='relative bg-gradient-to-r from-blue-500 to bg-indigo-600 dark:from-gray-800 dark:to-gray-900 py-24 px-4 text-center'>
         <div className='max-2-3xl mx-auto'>
         <h1 className='text-white text-4xl font-bold mb-4'>Find the Best courses for you </h1>
         <p className='text-gray-200 dark:text-gray-400 mb-8'>Discover, Learn and Upskill our wide range of courses</p>
@@ -16,6 +16,7 @@ const HeroSection = () => {
           />
           <Button className="bg-blue-600 dark:bg-blue-700 text-white px-6 py-3 rounded-r-full hover:bg-blue-700 dark:hover:bg-blue-800">Search</Button>
         </form>
+        <Button className='bg-white dark:bg-gray-800 text-blue-600 rounded-full hover:bg-gray-200 '>Explore Courses</Button>
         </div>
     </div>
   )
